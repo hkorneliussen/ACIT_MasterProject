8d3be2cb068ba5a1af49cd2f1657e81d31ae8c4e
hkorneliussen <99089340+hkorneliussen@users.noreply.github.com>
2023-04-20 18:22:37 +0200

    Add files via upload

---
+++ source/diffusion_model.py
@@ -0,0 +1,332 @@
+'''
+source: https://github.com/keras-team/keras-cv/blob/master/keras_cv/models/stable_diffusion/diffusion_model.py
+'''
+
+#importing dependencies
+import tensorflow as tf
+from tensorflow import keras
+
+#import custom dependencies
+from layers.padded_conv2d import PaddedConv2D
+
+
+#defining a custom neural network architecture called DiffusionModel
+class DiffusionModel(keras.Model):
+    def __init__(
+        self, img_height, img_width, max_text_length, name=None, download_weights=True
+    ):
+        context = keras.layers.Input((max_text_length, 768))
+        t_embed_input = keras.layers.Input((320,))
+        latent = keras.layers.Input((img_height // 8, img_width // 8, 4))
+
+        #processing t_embed_input thorugh two dense layers to obtain t_emb
+        t_emb = keras.layers.Dense(1280)(t_embed_input)
+        t_emb = keras.layers.Activation("swish")(t_emb)
+        t_emb = keras.layers.Dense(1280)(t_emb)
+
+        '''
+        Defining a down-sampling flow, followed by a middle flow and an up-sampling flow. 
+        In each flow, the output of the previous layer is processed through ResBlock layers and SpatialTransformer layers. The resblock layers contain a combination of conv layers and activation functions. The spatialtransformer layers use attention mechanisms to align feature maps with the context
+        '''
+
+        #Defining a Downsampling flow
+        outputs = []
+        x = PaddedConv2D(320, kernel_size=3, padding=1)(latent)
+        outputs.append(x)
+
+        
+        for _ in range(2):
+            x = ResBlock(320)([x, t_emb])
+            x = SpatialTransformer(8, 40, fully_connected=False)([x, context])
+            outputs.append(x)
+        x = PaddedConv2D(320, 3, strides=2, padding=1)(x)  # Downsample 2x
+        outputs.append(x)
+
+        for _ in range(2):
+            x = ResBlock(640)([x, t_emb])
+            x = SpatialTransformer(8, 80, fully_connected=False)([x, context])
+            outputs.append(x)
+        x = PaddedConv2D(640, 3, strides=2, padding=1)(x)  # Downsample 2x
+        outputs.append(x)
+
+        for _ in range(2):
+            x = ResBlock(1280)([x, t_emb])
+            x = SpatialTransformer(8, 160, fully_connected=False)([x, context])
+            outputs.append(x)
+        x = PaddedConv2D(1280, 3, strides=2, padding=1)(x)  # Downsample 2x
+        outputs.append(x)
+
+        for _ in range(2):
+            x = ResBlock(1280)([x, t_emb])
+            outputs.append(x)
+
+        # Middle flow
+
+        x = ResBlock(1280)([x, t_emb])
+        x = SpatialTransformer(8, 160, fully_connected=False)([x, context])
+        x = ResBlock(1280)([x, t_emb])
+
+        # Upsampling flow
+
+        for _ in range(3):
+            x = keras.layers.Concatenate()([x, outputs.pop()])
+            x = ResBlock(1280)([x, t_emb])
+        x = Upsample(1280)(x)
+
+        for _ in range(3):
+            x = keras.layers.Concatenate()([x, outputs.pop()])
+            x = ResBlock(1280)([x, t_emb])
+            x = SpatialTransformer(8, 160, fully_connected=False)([x, context])
+        x = Upsample(1280)(x)
+
+        for _ in range(3):
+            x = keras.layers.Concatenate()([x, outputs.pop()])
+            x = ResBlock(640)([x, t_emb])
+            x = SpatialTransformer(8, 80, fully_connected=False)([x, context])
+        x = Upsample(640)(x)
+
+        for _ in range(3):
+            x = keras.layers.Concatenate()([x, outputs.pop()])
+            x = ResBlock(320)([x, t_emb])
+            x = SpatialTransformer(8, 40, fully_connected=False)([x, context])
+
+        # Exit flow
+
+        #applying a group normalization layer and an activation layer to the output of the up-sampling flow
+        x = keras.layers.GroupNormalization(epsilon=1e-5)(x)
+        x = keras.layers.Activation("swish")(x)
+        #the output is a 4-channel image produced by a padded convoluational layer
+        output = PaddedConv2D(4, kernel_size=3, padding=1)(x)
+
+        super().__init__([latent, t_embed_input, context], output, name=name)
+
+        #The model can load pre-trained weights by setting download_weights to True
+        
+        if download_weights:
+            diffusion_model_weights_fpath = "models/kcv_diffusion_model.h5"
+            self.load_weights(diffusion_model_weights_fpath)
+        
+        '''
+        if download_weights:
+            diffusion_model_weights_fpath = keras.utils.get_file(
+                origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/kcv_diffusion_model.h5",
+                file_hash="8799ff9763de13d7f30a683d653018e114ed24a6a819667da4f5ee10f9e805fe",
+            )
+            self.load_weights(diffusion_model_weights_fpath)
+        ''' 
+
+
+#Custom implementation of a Residual Block in keras
+class ResBlock(keras.layers.Layer):
+    #the output_dim argument defines the number of filters for the paddedconv2D layers
+    def __init__(self, output_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.output_dim = output_dim
+        #setting up 3 flows that contains a series of layers that perform various transformations on the input
+        self.entry_flow = [
+            keras.layers.GroupNormalization(epsilon=1e-5),
+            keras.layers.Activation("swish"),
+            PaddedConv2D(output_dim, 3, padding=1),
+        ]
+        self.embedding_flow = [
+            keras.layers.Activation("swish"),
+            keras.layers.Dense(output_dim),
+        ]
+        self.exit_flow = [
+            keras.layers.GroupNormalization(epsilon=1e-5),
+            keras.layers.Activation("swish"),
+            PaddedConv2D(output_dim, 3, padding=1),
+        ]
+
+    #build-method checks if the number of filters in the input tensor matches the specified output_dim..
+    def build(self, input_shape):
+        #if it is not, it creates a paddedconv2d layer to project the inputs to the desired number of filters
+        if input_shape[0][-1] != self.output_dim:
+            self.residual_projection = PaddedConv2D(self.output_dim, 1)
+        else:
+            self.residual_projection = lambda x: x
+
+    #defining the forward pass
+    def call(self, inputs):
+        #splitting the input argument into "inputs" and "embeddings"
+        inputs, embeddings = inputs
+        #setting the variable x equal to inputs
+        x = inputs
+        #starting a loop that iterates over the "entry_flow" list, which is a list of layers defined in the __init__ method
+        for layer in self.entry_flow:
+            #applying the current layer to the previous output 'x'
+            x = layer(x)
+        #starting a loop that iterates over the 'embedding_flow' list defined in the init method
+        for layer in self.embedding_flow:
+            #applying the current layer to the 'embeddings'
+            embeddings = layer(embeddings)
+        #adding the embeddings to x
+        x = x + embeddings[:, None, None]
+        #starting a loop that iterates over the exit_flow list layers, defined in the init method
+        for layer in self.exit_flow:
+            #applying the current layer to the previous output 'x'
+            x = layer(x)
+        #returining the sum of 'x' and the result of applying the residual_projection to the original inputs. 
+        #the residual_projection matches the dimension of the inputs and outputs of the block, so that they can be added together
+        return x + self.residual_projection(inputs)
+        
+        
+#defining a custom layer called Spatial transformer, which implements spatial transformer attention, which is used to perform spatial manipulation of feature maps
+class SpatialTransformer(keras.layers.Layer):
+    def __init__(self, num_heads, head_size, fully_connected=False, **kwargs):
+        super().__init__(**kwargs)
+        #initializing a groupnormalization layer
+        self.norm = keras.layers.GroupNormalization(epsilon=1e-5)
+        #calculating number of channels
+        channels = num_heads * head_size
+        #based on the fully_connected argument, initialize either ...
+        if fully_connected:
+            #...a dense layer 
+            self.proj1 = keras.layers.Dense(num_heads * head_size)
+        else:
+            #... a paddedconv2d layer
+            self.proj1 = PaddedConv2D(num_heads * head_size, 1)
+        #initializing basictransformer block class
+        self.transformer_block = BasicTransformerBlock(channels, num_heads, head_size)
+        if fully_connected:
+            self.proj2 = keras.layers.Dense(channels)
+        else:
+            self.proj2 = PaddedConv2D(channels, 1)
+
+    #defining the forward pass
+    def call(self, inputs):
+        inputs, context = inputs
+        _, h, w, c = inputs.shape
+        #the inputs are normalized using group normailzation
+        x = self.norm(inputs)
+        #the normalized inputs are transformed using a dense/paddedconv2D linear layer (depending on the value of fully_connected)
+        x = self.proj1(x)
+        #The output from the previous step is reshaped to a 3D tensor
+        x = tf.reshape(x, (-1, h * w, c))
+        #the reshaped output is processed by a basictransformerblock layer (custom layer)
+        x = self.transformer_block([x, context])
+        #the output from the previous step is reshaped bach to a 4D tensor 
+        x = tf.reshape(x, (-1, h, w, c))
+        #the output from proj2 (another dense/conv layer) is added to the original inputs, and returned 
+        return self.proj2(x) + inputs
+        
+        
+#Defining a block of a transformer architecture       
+class BasicTransformerBlock(keras.layers.Layer):
+    def __init__(self, dim, num_heads, head_size, **kwargs):
+        super().__init__(**kwargs)
+        #3 instances of layernormalization class are created and assigned to norm1, norm2, and norm3
+        #2 instances of crosattention class are created and assigned to attn1 and attn2
+        self.norm1 = keras.layers.LayerNormalization(epsilon=1e-5)
+        self.attn1 = CrossAttention(num_heads, head_size)
+        self.norm2 = keras.layers.LayerNormalization(epsilon=1e-5)
+        self.attn2 = CrossAttention(num_heads, head_size)
+        self.norm3 = keras.layers.LayerNormalization(epsilon=1e-5)
+        #an instance of GEGLU is created and assigned to geglu
+        self.geglu = GEGLU(dim * 4)
+        self.dense = keras.layers.Dense(dim)
+
+    #defining the forward pass
+    def call(self, inputs):
+        #splitting inputs into inputs and context
+        inputs, context = inputs
+        #x is assigned to the result of attn1 added to inputs
+        x = self.attn1([self.norm1(inputs), None]) + inputs
+        #x is assigned to the result of attn2 added to x
+        x = self.attn2([self.norm2(x), context]) + x
+        #the result of dense with geglu(norm3(x)) added to 'x' is returned
+        return self.dense(self.geglu(self.norm3(x))) + x
+        
+
+#defining a crossattention layer, that computes a cross-attention mechanism between the two inputs, referred to as 'inputs' and 'context'
+class CrossAttention(keras.layers.Layer):
+    #initializing the layer with num_heads and head_size
+    def __init__(self, num_heads, head_size, **kwargs):
+        super().__init__(**kwargs)
+        #3 dense layers are created, which are used to project the 'inputs' and 'context' into queries (q), keys (k) and values (v). 
+        self.to_q = keras.layers.Dense(num_heads * head_size, use_bias=False)
+        self.to_k = keras.layers.Dense(num_heads * head_size, use_bias=False)
+        self.to_v = keras.layers.Dense(num_heads * head_size, use_bias=False)
+        self.scale = head_size**-0.5
+        self.num_heads = num_heads
+        self.head_size = head_size
+        #creating a dense layer out_proj, used to project the final attention-weighted representation back to the original input size
+        self.out_proj = keras.layers.Dense(num_heads * head_size)
+
+    #defining the foward pass
+    def call(self, inputs):
+        inputs, context = inputs
+        #if 'context' is None, 'inputs' is used as the 'context'
+        context = inputs if context is None else context
+        #projecting the 'inputs' and 'context' into queries (q), keys (k) and values (v) using to_q, to_k and to_v dense laayers.
+        q, k, v = self.to_q(inputs), self.to_k(context), self.to_v(context)
+        #reshaping q, k and v into 3D tensors with specified dimensions
+        q = tf.reshape(q, (-1, inputs.shape[1], self.num_heads, self.head_size))
+        k = tf.reshape(k, (-1, context.shape[1], self.num_heads, self.head_size))
+        v = tf.reshape(v, (-1, context.shape[1], self.num_heads, self.head_size))
+
+        #transposing the tensors to have the specified dimensions
+        q = tf.transpose(q, (0, 2, 1, 3))  # (bs, num_heads, time, head_size)
+        k = tf.transpose(k, (0, 2, 3, 1))  # (bs, num_heads, head_size, time)
+        v = tf.transpose(v, (0, 2, 1, 3))  # (bs, num_heads, time, head_size)
+
+        #finding the dot product of q and k, and passing it thorugh a softmax function to compute attention weights
+        score = td_dot(q, k) * self.scale
+        weights = keras.activations.softmax(score)  # (bs, num_heads, time, time)
+        #the attention-weighted representation is computed by taking a dot product of the attention weights and the v-tensor
+        attn = td_dot(weights, v)
+        attn = tf.transpose(attn, (0, 2, 1, 3))  # (bs, time, num_heads, head_size)
+        #the final attention-weighted representation is reshaped and passed through the out_proj dense layer to get the final output
+        out = tf.reshape(attn, (-1, inputs.shape[1], self.num_heads * self.head_size))
+        return self.out_proj(out)
+        
+
+#definign an upsample class
+class Upsample(keras.layers.Layer):
+    def __init__(self, channels, **kwargs):
+        super().__init__(**kwargs)
+        #an instance of the upsampling2d layer with a factor of 2, meaning that the iamge will be upsampled by a factor of 2 in both height and witdth
+        self.ups = keras.layers.UpSampling2D(2)
+        #an instance of a 2D conv layer with 'channels' number of output filters and a kernel size of 3, with padding set to 1
+        self.conv = PaddedConv2D(channels, 3, padding=1)
+
+    #defining forward pass
+    def call(self, inputs):
+        #upsampling the input using self.ups(inputs). 
+        #the result of the upsampling is passed to self.conf
+        #the result of the conv operation is returned as the output of the call method
+        return self.conv(self.ups(inputs))
+        
+'''
+GEGLU = Gatet Exponential Linear Unit
+used as a non-linear activation function
+'''
+        
+#Defining a custom keras layer
+class GEGLU(keras.layers.Layer):
+    def __init__(self, output_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.output_dim = output_dim
+        #creating a dense layer with 'output_dim*2'output units
+        self.dense = keras.layers.Dense(output_dim * 2)
+
+    #defining the forward pass, the 'inputs' argument is the data being passed thorugh the layer
+    def call(self, inputs):
+        #applying the defined dense layer to the input data inputs
+        x = self.dense(inputs)
+        #splitting up the output of the dense layer into x (the first output_dim values and gate (remaining values)
+        x, gate = x[..., : self.output_dim], x[..., self.output_dim :]
+        #calcullating the tanh-activation of the expression
+        tanh_res = keras.activations.tanh(
+            gate * 0.7978845608 * (1 + 0.044715 * (gate**2))
+        )
+        #returns the final output of the GEGLU layer
+        return x * 0.5 * gate * (1 + tanh_res)
+        
+#Defining function that performs a dot product between to tensors 'a' and 'b'
+def td_dot(a, b):
+    aa = tf.reshape(a, (-1, a.shape[2], a.shape[3]))
+    bb = tf.reshape(b, (-1, b.shape[2], b.shape[3]))
+    cc = keras.backend.batch_dot(aa, bb)
+    #the output is a scalar value when applied to two vectors, and a matrix when applied to two matrices
+    return tf.reshape(cc, (-1, a.shape[1], cc.shape[1], cc.shape[2]))
---
+++ source/text_encoder.py
@@ -0,0 +1,178 @@
+'''
+source: https://github.com/keras-team/keras-cv/blob/master/keras_cv/models/stable_diffusion/text_encoder.py
+'''
+
+#importing dependencies
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.experimental import numpy as tfnp
+
+'''
+The textencoder class is a keras.model that is used to produce an embedded representation of a text input. 
+
+Its input tokens and positions are passed thorugh an CLIPEmbedding layer, which is used to embed the tokens and positions into a continous vector space. The resulting embedded representation is then passed through multiple CLIPEncoder layers' to produce the final embedded representation. The final representation is then passed through a LayerNormalization layer to normalize the activations. 
+
+If download_weights is true, the weights of the model will be downloaded and loaded. 
+'''
+
+#Defining text encoder class 
+class TextEncoder(keras.Model):
+    #the class takes two inputs; tokens and positions
+    #it outputs an embedded representation of the tokens
+    def __init__(self, max_length, vocab_size=49408, name=None, download_weights=True):
+        tokens = keras.layers.Input(shape=(max_length,), dtype="int32", name="tokens")
+        positions = keras.layers.Input(
+            shape=(max_length,), dtype="int32", name="positions"
+        )
+        #passing the inputs through an embedding layer
+        x = CLIPEmbedding(vocab_size, 768, max_length)([tokens, positions])
+        #passing it through multiple CLIPEncoder layers to produce the final embedded representation
+        for _ in range(12):
+            x = CLIPEncoderLayer(768, 12, activation=quick_gelu)(x)
+        embedded = keras.layers.LayerNormalization(epsilon=1e-5)(x)
+        super().__init__([tokens, positions], embedded, name=name)
+        #If download weights is true, the weights for the model will be downloaded from a URL and loaded into the model
+        
+        if download_weights:
+            text_encoder_weights_fpath = 'models/kcv_encoder.h5'
+            self.load_weights(text_encoder_weights_fpath)
+        
+        '''
+        if download_weights:
+            text_encoder_weights_fpath = keras.utils.get_file(
+                origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/kcv_encoder.h5",
+                file_hash="4789e63e07c0e54d6a34a29b45ce81ece27060c499a709d556c7755b42bb0dc4",
+            )
+            self.load_weights(text_encoder_weights_fpath)
+        '''    
+            
+#Defining the Gaussian Error Linear Unit (GELU) activation function           
+def quick_gelu(x):
+    #GELU is a smooth approximation of the ReLU activation
+    return x * tf.sigmoid(x * 1.702)
+    
+'''
+Token embeddings are used to represent the meaning of the words in the input sequence. They capture the semantic information of the words. 
+
+Position embeddings are used to represent the position of the words in the input sequence. The idea is that the same word can have different meanings in different positions in a sentence, and that the position of the word can provide additional information to the model. Position embeddings are added to the token embeddings to provide the model with information about the position of the words. 
+'''
+    
+#Defining a custom layer in keras called CLIPEmbedding
+#it takes two inputs; tokens and positions, and concatenates their embeddings to produce a final embedding representation
+class CLIPEmbedding(keras.layers.Layer):
+    def __init__(self, input_dim=49408, output_dim=768, max_length=77, **kwargs):
+        super().__init__(**kwargs)
+        #creating a token_embedding using an embedding layer
+        self.token_embedding = keras.layers.Embedding(input_dim, output_dim)
+        #creating a position_embedding using another embedding laer
+        self.position_embedding = keras.layers.Embedding(max_length, output_dim)
+
+    #Defining the foward pass thorugh the layer
+    def call(self, inputs):
+        tokens, positions = inputs
+        tokens = self.token_embedding(tokens)
+        positions = self.position_embedding(positions)
+        #Final embedding representation s obtained by element-wise adding the token and position embeddings
+        return tokens + positions
+        
+
+#defiing a single layer, CLIPEncoderLayer, in a multi-layer transformer-based architecture       
+class CLIPEncoderLayer(keras.layers.Layer):
+    #the CLIPEncoderLayer consists of several components
+    def __init__(self, embed_dim, num_heads, activation=None, **kwargs):
+        super().__init__(**kwargs)
+        #Normalization layer that normalizes the input tensor to have mean 0 and variance 1
+        self.layer_norm1 = keras.layers.LayerNormalization(epsilon=1e-5)
+        #Layer that performs self-attention on the input tensor
+        self.clip_attn = CLIPAttention(embed_dim, num_heads, causal=True)
+        self.layer_norm2 = keras.layers.LayerNormalization(epsilon=1e-5)
+        #2 fully-connected (dense) layers that perform linear transformations on the input tensor 
+        self.fc1 = keras.layers.Dense(embed_dim * 4)
+        self.fc2 = keras.layers.Dense(embed_dim)
+        self.activation = activation
+
+    #defining the forward pass of the layer
+    def call(self, inputs):
+        residual = inputs
+        #passing the inputs thourgh the layer_norm1 layer
+        x = self.layer_norm1(inputs)
+        #passing the inputs through the clip-attn layer
+        x = self.clip_attn(x)
+        #adding the results of the self-attention to the residual connection
+        x = residual + x
+        residual = x
+        #passing the input tensor thorugh layer_norm2 layer
+        x = self.layer_norm2(x)
+        #passing it through 2 dense layers and an activation function
+        x = self.fc1(x)
+        x = self.activation(x)
+        x = self.fc2(x)
+        #the results is added to the residual connection and returned as the output of the layer
+        return x + residual
+        
+'''
+The CLIPAttention layer is a custom implementation of the attention mechanism in deep learning models. It takes inputs and rturns the attention-weighted representation of the inputs. 
+
+The attention mechanism calculates a weight for each input and combines the inputs based on their weights. In this specific implementation, the attention mechanism is used for multi-head self-attention. This means that the layer takes the inputs, projects them into multiple attention heads, and uses each head to attent to different parts of the inputs. The results from each head are then concatenated and transformed to produce the final output
+'''
+        
+#Defining a custom layer called CLIPAttention
+class CLIPAttention(keras.layers.Layer):
+    #setting up default values and creates some dense layers that will be used in the attention computation
+    def __init__(self, embed_dim=768, num_heads=12, causal=True, **kwargs):
+        super().__init__(**kwargs)
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.causal = causal
+        self.head_dim = self.embed_dim // self.num_heads
+        self.scale = self.head_dim**-0.5
+        self.q_proj = keras.layers.Dense(self.embed_dim)
+        self.k_proj = keras.layers.Dense(self.embed_dim)
+        self.v_proj = keras.layers.Dense(self.embed_dim)
+        self.out_proj = keras.layers.Dense(self.embed_dim)
+
+    #taking the inputs and reutnring a reshaped version of the inputs
+    def reshape_states(self, x, sequence_length, batch_size):
+        x = tf.reshape(x, (batch_size, sequence_length, self.num_heads, self.head_dim))
+        return tf.transpose(x, (0, 2, 1, 3))  # bs, heads, sequence_length, head_dim
+
+    #implementing the attention computation
+    def call(self, inputs, attention_mask=None):
+        #if an attention is not provided, it creates a triangular attention mask that sets attention scores for positions to the right of the target sequence to negative infinity
+        if attention_mask is None and self.causal:
+            length = tf.shape(inputs)[1]
+            attention_mask = tfnp.triu(
+                tf.ones((1, 1, length, length), dtype=self.compute_dtype) * -tfnp.inf,
+                k=1,
+            )
+        
+        #the attention is computed by projecting the inputs into queries, keys and values, reshaping these states and computing the dot product of the query states and the transpose of the key states. 
+        _, tgt_len, embed_dim = inputs.shape
+        query_states = self.q_proj(inputs) * self.scale
+        key_states = self.reshape_states(self.k_proj(inputs), tgt_len, -1)
+        value_states = self.reshape_states(self.v_proj(inputs), tgt_len, -1)
+
+        proj_shape = (-1, tgt_len, self.head_dim)
+        query_states = self.reshape_states(query_states, tgt_len, -1)
+        query_states = tf.reshape(query_states, proj_shape)
+        key_states = tf.reshape(key_states, proj_shape)
+
+        src_len = tgt_len
+        value_states = tf.reshape(value_states, proj_shape)
+        attn_weights = query_states @ tf.transpose(key_states, (0, 2, 1))
+
+        attn_weights = tf.reshape(attn_weights, (-1, self.num_heads, tgt_len, src_len))
+        attn_weights = attn_weights + attention_mask
+        attn_weights = tf.reshape(attn_weights, (-1, tgt_len, src_len))
+
+        #softmax is applied to the produced attention scores, which are used to compute the weighted sum of the value states
+        attn_weights = tf.nn.softmax(attn_weights)
+        attn_output = attn_weights @ value_states
+
+        attn_output = tf.reshape(
+            attn_output, (-1, self.num_heads, tgt_len, self.head_dim)
+        )
+        attn_output = tf.transpose(attn_output, (0, 2, 1, 3))
+        attn_output = tf.reshape(attn_output, (-1, tgt_len, embed_dim))
+        #the final attention output is returned after passing it thorugh the out_proj dense layer
+        return self.out_proj(attn_output)
