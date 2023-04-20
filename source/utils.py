9f87b780ae1bd6ce031a669f5b6375209cf521b9
hkorneliussen <99089340+hkorneliussen@users.noreply.github.com>
2023-04-20 18:12:49 +0200

    Update utils.py

--- source/utils.py
+++ source/utils.py
@@ -10,7 +10,6 @@ from datetime import date
 from time import mktime
 import numpy as np   
 from transformers import pipeline
-import requests
 
 import openai
 import random
@@ -160,31 +159,4 @@ def summarize(sentence):
     
     return summary
     
-def download_file_from_google_drive(id, destination):
-    def get_confirm_token(response):
-        for key, value in response.cookies.items():
-            if key.startswith('download_warning'):
-                return value
 
-        return None
-
-    def save_response_content(response, destination):
-        CHUNK_SIZE = 32768
-
-        with open(destination, "wb") as f:
-            for chunk in response.iter_content(CHUNK_SIZE):
-                if chunk: # filter out keep-alive new chunks
-                    f.write(chunk)
-
-    URL = "https://docs.google.com/uc?export=download"
-
-    session = requests.Session()
-
-    response = session.get(URL, params = { 'id' : id }, stream = True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
-
-    save_response_content(response, destination)    
