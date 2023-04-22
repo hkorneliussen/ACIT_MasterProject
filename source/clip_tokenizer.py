a2654a4e37a3b0b916db352fad82992b1e5cc105
hkorneliussen <99089340+hkorneliussen@users.noreply.github.com>
2023-04-22 10:06:32 +0200

    Update clip_tokenizer.py

--- source/clip_tokenizer.py
+++ source/clip_tokenizer.py
@@ -1,4 +1,4 @@
-# Copyright 2022 The KerasCV Authors
+# Copyright 2023 Hanne Korneliussen
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
